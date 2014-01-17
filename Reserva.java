34f85fda3c8ec4af938d30f3018ba778c797c3d9
Rafael Levi <rafaellevissa@hotmail.com>
2014-01-17 04:09:28 -0200

    Create Reserva.java

---
+++ Reserva.java
@@ -0,0 +1,11 @@
+public class Reserva {
+    private Usuario usuario;
+    private Material material;
+    
+    public Usuario getUsuario(){
+        return usuario;
+    }
+    public Material getMaterial(){
+        return material;
+    }
+}
