20306b4d7b1e2e0240fe8dd9851a6c6e661a1642
Rafael Levi <rafaellevissa@hotmail.com>
2014-01-17 04:07:59 -0200

    Create Livro.java

---
+++ Livro.java
@@ -0,0 +1,19 @@
+public class Livro {
+    private String editora;
+    private String autores;
+    private int edicao;
+    private int ano;
+    
+    public String getEditora(){
+        return editora;
+    }
+    public String getAutores(){
+        return autores;
+    }
+    public int getEdicao(){
+        return edicao;
+    }
+    public int getAno(){
+        return ano;
+    }
+}
