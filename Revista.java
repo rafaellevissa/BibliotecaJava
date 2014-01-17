f4276045e127ff9840d5365208880b780b407876
Rafael Levi <rafaellevissa@hotmail.com>
2014-01-17 04:10:02 -0200

    Create Revista.java

---
+++ Revista.java
@@ -0,0 +1,11 @@
+public class Revista {
+    private int edicao;
+    private String mesAno;
+    
+    public int getEdicao(){
+        return edicao;
+    }
+    public String getMesAno(){
+        return mesAno;
+    }
+}
