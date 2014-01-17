5f0927033f4cb629f9587a6b3f3ecb7c33b16b12
Rafael Levi <rafaellevissa@hotmail.com>
2014-01-17 04:06:46 -0200

    Create Dvd.java

---
+++ Dvd.java
@@ -0,0 +1,15 @@
+public class Dvd {
+    private String autores;
+    private int ano;
+    private String regiao;
+    
+    public String getAutores(){
+        return autores;
+    }
+    public int getAno(){
+        return ano;
+    }
+    public String getRegiao(){
+        return regiao;
+    }
+}
