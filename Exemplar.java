3831711b8f31cf3dbb9747edf92c2506100cf089
Rafael Levi <rafaellevissa@hotmail.com>
2014-01-17 04:07:38 -0200

    Create Exemplar.java

---
+++ Exemplar.java
@@ -0,0 +1,12 @@
+public class Exemplar {
+    private String codigo;
+    private Material material;
+    
+    public String getCodigo(){
+        return codigo;
+    }
+    public Material getMaterial(){
+        return material;
+    }
+    
+}
