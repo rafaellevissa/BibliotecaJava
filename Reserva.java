6a554075db6778bbaf9c511cd845590669c33181
Rafael Levi <rafaellevissa@hotmail.com>
2014-01-26 10:06:30 -0200

    Update Reserva.java

--- Reserva.java
+++ Reserva.java
@@ -1,3 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblioteca;
+
+/**
+ *
+ * @author levi
+ */
 public class Reserva {
     private Usuario usuario;
     private Material material;
