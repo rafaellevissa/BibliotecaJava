da7ccbae48f44dbea5a471672645ffaa1171653a
Rafael Levi <rafaellevissa@hotmail.com>
2014-01-26 10:06:12 -0200

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -1,6 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblioteca;
+
+/**
+ *
+ * @author levi
+ */
 public class Professor extends Usuario{
     @Override
     public boolean emprestar(){
         return true;
+    }
+    public Professor(int codigo, String nome){
+        super(codigo,nome);
     }
 }
