1fa502ba61fc74e4095a5e91156dbf22e97fdfa9
Rafael Levi <rafaellevissa@hotmail.com>
2014-01-26 10:03:39 -0200

    Update Emprestimo.java

--- Emprestimo.java
+++ Emprestimo.java
@@ -1,3 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblioteca;
+import java.util.*;
+/**
+ *
+ * @author levi
+ */
 public class Emprestimo {
     private Exemplar exemplar;
     private Usuario usuario;
