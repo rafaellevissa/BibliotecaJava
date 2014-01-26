18991eb9547ab047052d4843123017a663c807ac
Rafael Levi <rafaellevissa@hotmail.com>
2014-01-26 10:06:47 -0200

    Update Revista.java

--- Revista.java
+++ Revista.java
@@ -1,11 +1,31 @@
-public class Revista {
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblioteca;
+
+/**
+ *
+ * @author levi
+ */
+public class Revista extends Material {
     private int edicao;
-    private String mesAno;
+    private String mes;
+    private int ano;
     
     public int getEdicao(){
         return edicao;
     }
-    public String getMesAno(){
-        return mesAno;
+    public String getMes(){
+        return mes;
+    }
+    public int getAno(){
+        return ano;
+    }
+    public Revista(int codigo, String titulo,int edicao, String mes, int ano){
+        super(codigo, titulo);
+        this.edicao=edicao;
+        this.mes=mes;
+        this.ano=ano;
     }
 }
