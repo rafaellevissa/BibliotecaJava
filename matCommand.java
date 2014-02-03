3f90fa6d2bca9000cd8e75bf243ec4b1a2593aa7
Rafael Levi <rafaellevissa@hotmail.com>
2014-02-03 13:12:08 -0200

    Create matCommand.java

---
+++ matCommand.java
@@ -0,0 +1,10 @@
+package BibliotecaJava;
+
+public class matCommand implements Command {
+    
+    @Override
+    public void execute(Object arg1,Object arg2) {
+       //
+      System.out.println("acao de consulta de material.");  
+    }
+}
