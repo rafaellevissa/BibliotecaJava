b4c32ff5bbc4bad9185443ae319c745382b54129
Rafael Levi <rafaellevissa@hotmail.com>
2014-02-03 13:03:06 -0200

    Update AlunoPos.java

--- AlunoPos.java
+++ AlunoPos.java
@@ -1,13 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package biblioteca;
-
-/**
- *
- * @author levi
- */
+package BibliotecaJava;
 public class AlunoPos extends Usuario{
     @Override
     public boolean emprestar(){
