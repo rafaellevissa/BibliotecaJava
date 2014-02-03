4e84e9ac42854a6877b3b1fd70cecda1999e7b4e
Rafael Levi <rafaellevissa@hotmail.com>
2014-02-03 13:02:33 -0200

    Update AlunoGraduacao.java

--- AlunoGraduacao.java
+++ AlunoGraduacao.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package biblioteca;
 
 /**
@@ -15,5 +11,8 @@ public class AlunoGraduacao extends Usuario{
     }
     public AlunoGraduacao(int codigo, String nome){
         super(codigo,nome);
+    }
+    public RealizarEmprestimo(int CodEzemplar){
+        
     }
 }
