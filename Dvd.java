658f09db1275a38e369e5e2c8af0cc68d76d57cc
Rafael Levi <rafaellevissa@hotmail.com>
2014-02-03 13:06:12 -0200

    Update Dvd.java

--- Dvd.java
+++ Dvd.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package biblioteca;
+package BibliotecaJava;
 
-/**
- *
- * @author levi
- */
 public class Dvd extends Material{
     private String autores;
     private int ano;
@@ -23,7 +15,7 @@ public class Dvd extends Material{
         return regiao;
     }
     public Dvd (int codigo, String titulo, String autores, int ano, int regiao){
-        super(codigo,titulo);
+        super(codigo,titulo,"DVD");
         this.ano=ano;
         this.autores=autores;
         this.regiao=regiao;
