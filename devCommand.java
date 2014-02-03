d681ad72113201f7d4d555f2bfc1b099e02ffc0d
Rafael Levi <rafaellevissa@hotmail.com>
2014-02-03 13:05:29 -0200

    Create devCommand.java

---
+++ devCommand.java
@@ -0,0 +1,10 @@
+package BibliotecaJava;
+
+public class devCommand implements Command {
+    
+    @Override
+    public void execute(Object arg1,Object arg2) {
+       //
+      System.out.println("acao de devoluÃ§Ã£o.");  
+    }
+}
